e5dd444a77e16bd16b5c9c4930cd3879f447035e
R <Vikas_Raju@Dell.com>
2020-04-25 14:24:44 +0530

    base setup,dependencies,home

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+    res.send("home")
+})
+
+app.listen(PORT, () => {
+    console.log("hello world")
+});
